0adc0d9496c7b2d43d8582fbbb3db2738ec3ee55
Kamal Parashar <kamalparasharp1123@gmail.com>
2024-11-16 00:28:22 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import authService from './firebase/auth.js'
+import {login, logout} from './store/authSlice.js'
+import {Outlet} from 'react-router-dom'
+import {collection, onSnapshot, query, orderBy } from "firebase/firestore"
+import { db } from "./firebase/firebase.js"
+import {Header} from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  const authStatus = useSelector(state=>state.auth.status)
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        console.log("userdata");
+        dispatch(login({userData}));
+      }
+      else{
+        console.log("no data");
+        dispatch(logout());
+      }
+    })
+    .catch((error) => {
+      console.log("NO user currently logged In");
+      console.log("error in UseEffect:",error);
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  useEffect(() => {
+    const colref = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    return onSnapshot(colref, (snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header />
+        <main className='min-h-screen flex justify-center items-center font-semibold md:text-lg lg:text-xl'>
+          <Outlet />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+    ) : null
 }
 
-export default App
+export default App 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { AuthLayout, Login } from "./components/index.js";
+import Signup from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <AuthLayout>
+            <Home/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout >
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout >
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+);
