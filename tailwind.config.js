7d55c40c8bcdcc55eaf11efaeab15edb8be216b0
Kamal Parashar <kamalparasharp1123@gmail.com>
2024-11-16 00:29:01 +0530

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': {'min': '300px', 'max': '426px'},
+
+      'md': {'min': '426px', 'max': '1023px'},
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+
+      'xl': {'min': '1279px', 'max': '1535px'},
+
+      '2xl': {'min': '1536px'},
+    },
   },
   plugins: [],
 }
